ad2810a16b02c4b28b08a5d30febf52ba675d4cd
evaseemefly <evaseemefly@126.com>
2017-07-18 22:19:55 +0800

    7月18日家中修改

--- QuickPositioning.py
+++ QuickPositioning.py
@@ -2,3 +2,12 @@ import ArrayOper
 def getPosition(lat,lon,lat_array,lon_array):
     """根据传入的经纬度数组获取在lat与lon两个数组中的序号
     """
+    value_max_lat=max(lat_array)
+    value_min_lat=max(lat_array)
+    totallen=abs(value_max_lat-value_min_lat)
+     #获取lat数组的间隔
+    set_lat_interval= ArrayOper.getArrayInterval(lat_array)
+    # 获取长度
+    index_len=len(lat_array)
+    if len(set_lat_interval)<=0:
+        
